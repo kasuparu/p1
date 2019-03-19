650a0d72122ec220965f0e500e54de58be9428a4
kasuparu <kasuparu@about.me>
2019-03-19 15:52:49 +0200

    TimestampFile error messages & format

--- src/service/timestamp-file.js
+++ src/service/timestamp-file.js
@@ -4,27 +4,27 @@ const ENCODING = 'utf8';
 
 let timestamp;
 
-export const loadTimestamp = (cb) => {
-  fs.readFile(FILENAME, 'utf8', (err, stringFromFile) => {
-    if (error) {
-      return cb(error);
-    }
+module.exports.loadTimestamp = (cb) => {
+    fs.readFile(FILENAME, ENCODING, (error, stringFromFile) => {
+        if (error) {
+            return cb(error);
+        }
 
-    const parsedTimestamp = parseInt(stringFromFile, 10);
-    if (isNan(parsedTimestamp)) {
-      return cb(new Error('timestamp file contains NaN'));
-    }
+        const parsedTimestamp = parseInt(stringFromFile, 10);
+        if (isNaN(parsedTimestamp)) {
+            return cb(new Error('Timestamp file contains NaN'));
+        }
 
-    timestamp = parsedTimestamp;
-    return cb(null, timestamp);
-  });
+        timestamp = parsedTimestamp;
+        return cb(null, timestamp);
+    });
 };
 
-export const get = () => timestamp;
+module.exports.get = () => timestamp;
 
-export const saveTimestamp = (newTimestamp, cb) => {
-  timestamp = newTimestamp;
-  // TODO Deal with multiple saves at the same time
-  // It's unsafe to call multiple writeFile
-  fs.writeFile(FILENAME, newTimestamp, cb);
+module.exports.saveTimestamp = (newTimestamp, cb) => {
+    timestamp = newTimestamp;
+    // TODO Deal with multiple saves at the same time
+    // It's unsafe to call multiple writeFile
+    fs.writeFile(FILENAME, newTimestamp, cb);
 };
